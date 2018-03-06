49743a29bcf6a63e5147faa0baac47c63b56a14b
chris.cc.ng <Chris.CC.Ng@pccw.com>
2018-03-06 17:34:34 +0800

    v1

--- AngularJSCRUD/AngularJS_users/Services/IndexServices.js
+++ AngularJSCRUD/AngularJS_users/Services/IndexServices.js
@@ -2,7 +2,7 @@
 
     var getPersons = function () {
         //return $http.get("http://127.0.0.1:8081/getPersons");
-        return $http.get("http://10.89.73.15:3000/api/user");
+        return $http.get("https://quiet-brushlands-18479.herokuapp.com/api/user");
         //var url = "data.txt";   
         //return $http.get(url).success( function(response) {
            //$scope.listPerson = response; 
@@ -13,7 +13,7 @@
         var parameters = {
             user_id: user_id,
         };
-        return $http.get("http://10.89.73.15:3000/api/user/"+user_id);
+        return $http.get("https://quiet-brushlands-18479.herokuapp.com/api/user/"+user_id);
     }
 
     var addPerson = function (obj) {
@@ -27,7 +27,7 @@
         };
         //return $http.get("http://127.0.0.1:8081/addPerson", config);
         console.log('Data send from AngularJS addPerson: '+JSON.stringify(obj));
-        return $http.post("http://10.89.73.15:3000/api/user", obj);
+        return $http.post("https://quiet-brushlands-18479.herokuapp.com/api/user", obj);
     }    
     var editPerson = function (obj) {
         //removed the $$hashKey properties
@@ -40,7 +40,7 @@
         };
         //return $http.get("http://127.0.0.1:8081/updatePerson", config);
         console.log('Data send from AngularJS editPerson: '+JSON.stringify(obj));
-        return $http.put("http://10.89.73.15:3000/api/user/"+obj.user_id, obj);
+        return $http.put("https://quiet-brushlands-18479.herokuapp.com/api/user/"+obj.user_id, obj);
     }
     var deletePerson = function (id) {
         var parameters = {
@@ -50,7 +50,7 @@
             params: parameters
         };
         //return $http.get("http://127.0.0.1:8081/removePerson", config);
-        return $http.delete("http://10.89.73.15:3000/api/user/"+id, config);
+        return $http.delete("https://quiet-brushlands-18479.herokuapp.com/api/user/"+id, config);
     }
     
     return {
